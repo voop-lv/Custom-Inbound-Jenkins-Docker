65aaf0055368252199ac6a2506b8efc7ca542d0b
Voop <viperlv2@inbox.lv>
2022-08-23 10:35:06 +0300

    Package listing fix

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ apt-get update
 apt-get full-upgrade
 
 echo "Installing custom packages!"
-apt-get install sshpass lftp python3 python2 python python-pip python3-pip -y
+apt-get install sshpass lftp python3 python3-pip gcc -y
 echo "End of install script!"
