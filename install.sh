c319aff682bf0f169071d2280ddbbe5438daff5c
Voop <viperlv2@inbox.lv>
2022-11-26 19:33:53 +0200

    Updated the build/install script

--- install.sh
+++ install.sh
@@ -6,3 +6,7 @@ apt-get full-upgrade
 echo "Installing custom packages!"
 apt-get install sshpass lftp python3 python3-pip gcc -y
 echo "End of install script!"
+
+echo "Installing python packages!"
+pip install hvac pysftp texttable docker boto3
+echo "End of installing python packages!"
