a796c57072a5650e6a45dcbd4301607e178595b3
Voop <viperlv2@inbox.lv>
2023-09-05 18:48:57 +0300

    Updated Install script

--- install.sh
+++ install.sh
@@ -8,5 +8,5 @@ apt-get install sshpass lftp python3 python3-pip gcc -y
 echo "End of install script!"
 
 echo "Installing python packages!"
-pip install hvac pysftp texttable docker boto3
+pip install hvac pysftp texttable docker boto3 bcrypt certifi cffi charset-normalizer idna numpy opencv-python requests urllib3 paramiko PyNaCl pyhcl pycparser
 echo "End of installing python packages!"
