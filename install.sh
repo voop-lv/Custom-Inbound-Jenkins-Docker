245459d634895eb3f6c727436e01f6dfa027989b
Voop <viperlv2@inbox.lv>
2024-02-21 09:33:09 +0200

    Package fixes

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ apt-get update
 apt-get full-upgrade
 
 echo "Installing custom packages!"
-apt-get install sshpass lftp python3 python3-pip gcc -y
+apt-get install sshpass lftp python3 python3-pip gcc python3-full -y
 echo "End of install script!"
 
 echo "Installing python packages!"
-pip install hvac pysftp texttable docker boto3 bcrypt certifi cffi charset-normalizer idna numpy opencv-python requests urllib3 paramiko PyNaCl pyhcl pycparser
+pip install hvac pysftp texttable docker boto3 bcrypt certifi cffi charset-normalizer idna numpy opencv-python requests urllib3 paramiko PyNaCl pyhcl pycparser --break-system-packages
 echo "End of installing python packages!"
